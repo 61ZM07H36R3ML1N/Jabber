bdd65157abf019be816025a340856ec3995c9224
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-11-08 21:57:59 -0800

    chunk commit

---
+++ .history/jabber/src/Sidebar_20221108214726.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Sidebar.css';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+    return (
+      <div className="sidebar">
+        {/* Jabber icon */}
+        <TwitterIcon />
+
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+      </div>
+    );
+}
+
+export default Sidebar;
