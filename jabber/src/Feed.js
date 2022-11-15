3a11a531ab0837c1329478be76a83f3c868a2b1c
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-11-15 12:13:27 -0800

    flexing display elements

---
+++ jabber/src/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Feed() {
+  return (
+    <div>
+        {/* Header */}
+        <h2>Home</h2>
+
+
+        {/* JabberBox */}
+
+
+        {/* Post */}
+    </div>
+  )
+}
+
+export default Feed
