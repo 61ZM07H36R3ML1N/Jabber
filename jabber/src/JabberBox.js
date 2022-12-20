8cf95ca3de43e7b638ad6eb225508a52c4957177
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-12-19 20:22:33 -0800

    code chunk

---
+++ jabber/src/JabberBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./JabberBox.css"
+import { Avatar, Button, Input } from "@material-ui/core"
+
+function JabberBox() {
+  return (
+    <div className='jabberBox'>
+        <form>
+            <div className='jabberBox__input'>
+
+            </div>
+            <Button>Jabber</Button>
+        </form>
+    </div>
+  )
+}
+
+export default JabberBox
