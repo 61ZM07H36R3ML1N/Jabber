3746dbceb07bf0dc5a3dab400c6b6f63f951f466
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-12-26 02:56:16 -0800

    formatting and minor code changes

--- jabber/src/Post.js
+++ jabber/src/Post.js
@@ -1,27 +1,32 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import './Post.css';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 
-function Post() {
-  return (
-    <div className="Post">
-      <div className="post_avatar">
-        <Avatar src=""></Avatar>
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="Post" ref={ref}>
+        <div className="post_avatar">
+          <Avatar src={avatar}></Avatar>
+        </div>
+        <div className="post__body"></div>
+        <div className="post__header"></div>
+        <div className="post__headerText">
+          <h3>
+            {displayName}{' '}
+            <span className="post__headerSpecial">
+              {verified && <VerifiedUserIcon className="post__badge" />} @
+              {username}
+            </span>
+          </h3>
+        </div>
+        <div className="post__headerDescription">
+          <p> I challenge you to build a Twitter Clone with React.</p>
+        </div>
       </div>
-      <div className="post__body"></div>
-      <div className="post__header"></div>
-      <div className="post__headerText">
-        <h3>
-          Brian Jamiel{' '}
-          <span>
-            <VerifiedUserIcon className="post__badge" />
-          </span>
-        </h3>
-      </div>
-      <div className="post__headerDescription"></div>
-    </div>
-  );
-}
+    );
+  },
+);
 
 export default Post;
